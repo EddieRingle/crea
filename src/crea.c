3ea2d833d477ffce1c5bbed3b421b2d56a20c618
Eddie Ringle <eddie@eringle.net>
2014-10-06 16:15:45 -0400

    Add support for parsing dependencies
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/crea.c
+++ src/crea.c
@@ -17,6 +17,23 @@ typedef struct source {
     struct source *next, *prev;
 } source_t;
 
+typedef struct external_data {
+    char *file;
+} external_data_t;
+
+typedef struct dependency {
+    char *name;
+
+    int is_external;
+
+    union {
+        char *version;
+        external_data_t external;
+    };
+
+    struct dependency *next, *prev;
+} dependency_t;
+
 typedef struct crea {
     char *name;
     char *version;
@@ -24,7 +41,11 @@ typedef struct crea {
     source_t *sources;
     source_t *headers;
 
+    dependency_t *dependencies;
+
     char *cflags;
+
+    char *current_dependency;
 } crea_t;
 
 static int
@@ -44,6 +65,54 @@ crea_init(crea_t **crea)
     } while (0);
 
 static int
+add_to_dependencies(dependency_t **listp, dependency_t *dep)
+{
+    dependency_t *add = NULL;
+    dependency_t *tmp = NULL;
+
+    if (*listp == NULL) {
+        *listp = malloc(sizeof(dependency_t));
+        memcpy(*listp, dep, sizeof(dependency_t));
+        (*listp)->next = NULL;
+        (*listp)->prev = NULL;
+    } else {
+        for (tmp = *listp; tmp != NULL; tmp = tmp->next) {
+            if (!strcmp(dep->name, tmp->name)) {
+                LOGI("Attempted to add dependency with duplicate name: %s\n", dep->name);
+                return -1;
+            }
+            if (tmp->next == NULL) {
+                break;
+            }
+        }
+        add = malloc(sizeof(dependency_t));
+        memcpy(add, dep, sizeof(dependency_t));
+        add->next = NULL;
+        add->prev = tmp;
+        tmp->next = add;
+    }
+
+    return 0;
+}
+
+static void
+free_dependency_tree(dependency_t *tree)
+{
+    FREE_UNLESS_NULL(tree->name);
+    if (!tree->is_external) {
+        FREE_UNLESS_NULL(tree->version);
+    } else {
+        FREE_UNLESS_NULL(tree->external.file);
+    }
+    tree->prev = NULL;
+    if (tree->next != NULL) {
+        free_dependency_tree(tree->next);
+        tree->next = NULL;
+    }
+    free(tree);
+}
+
+static int
 add_to_sources(source_t **listp, char *path)
 {
     source_t *add = NULL;
@@ -60,7 +129,7 @@ add_to_sources(source_t **listp, char *path)
          */
         for (tmp = *listp; tmp != NULL; tmp = tmp->next) {
             if (!strcmp(path, tmp->path)) {
-                printf("Attempted to add duplicate source path: %s\n", path);
+                LOGI("Attempted to add duplicate source path: %s\n", path);
                 return -1;
             }
             if (tmp->next == NULL) {
@@ -95,20 +164,50 @@ crea_fini(crea_t **crea)
     FREE_UNLESS_NULL((*crea)->name);
     FREE_UNLESS_NULL((*crea)->version);
     FREE_UNLESS_NULL((*crea)->cflags);
+    FREE_UNLESS_NULL((*crea)->current_dependency);
     if ((*crea)->sources != NULL) {
         free_source_tree((*crea)->sources);
     }
     if ((*crea)->headers != NULL) {
         free_source_tree((*crea)->headers);
     }
+    if ((*crea)->dependencies != NULL) {
+        free_dependency_tree((*crea)->dependencies);
+    }
 
     return 0;
+}
+
+static dependency_t *
+create_dependency(char *name, char *version)
+{
+    dependency_t *res;
+
+    res = malloc(sizeof(dependency_t));
+    res->name = strdup(name);
+    res->version = strdup(version);
+    res->is_external = 0;
+
+    return res;
+}
+
+static dependency_t *
+create_external_dependency(char *name, char *file)
+{
+    dependency_t *res;
+
+    res = malloc(sizeof(dependency_t));
+    res->name = strdup(name);
+    res->external.file = strdup(file);
+    res->is_external = 1;
+
+    return res;
 }
 
 int done = 0;
 
 enum {
-    STATE_UNKNOWN,
+    STATE_UNKNOWN = 0,
     STATE_ROOT,
     STATE_AUTHORS,
     STATE_CONTRIBUTORS,
@@ -117,9 +216,32 @@ enum {
     STATE_DEPENDENCIES,
     STATE_CONFIGURATIONS,
 
+    STATE_DEPENDENCY,
+    STATE_EXTERNAL_DEP,
+
     STATE_NAME,
     STATE_VERSION,
-    STATE_CFLAGS
+    STATE_CFLAGS,
+    STATE_FILE
+};
+
+const char *state_strs[] = {
+    "UNKNOWN",
+    "ROOT",
+    "AUTHORS",
+    "CONTRIBUTORS",
+    "SOURCES",
+    "HEADERS",
+    "DEPENDENCIES",
+    "CONFIGURATIONS",
+
+    "DEPENDENCY",
+    "EXTERNAL_DEP",
+
+    "NAME",
+    "VERSION",
+    "CFLAGS",
+    "FILE"
 };
 
 int state_stack[250] = {STATE_UNKNOWN};
@@ -201,12 +323,18 @@ parse_file(const char *file)
                 pop_state();
                 break;
             case YAML_MAPPING_START_EVENT:
+                if (crea->current_dependency != NULL) {
+                    push_state(STATE_EXTERNAL_DEP);
+                }
                 if (last_scalar != NULL) {
                     free(last_scalar);
                 }
                 last_scalar = NULL;
                 break;
             case YAML_MAPPING_END_EVENT:
+                if (get_state() == STATE_EXTERNAL_DEP) {
+                    pop_state();
+                }
                 pop_state();
                 break;
             case YAML_ALIAS_EVENT:
@@ -232,6 +360,43 @@ parse_file(const char *file)
                             push_state(STATE_DEPENDENCIES);
                         } else if (!strcmp(event.data.scalar.value, "configurations")) {
                             push_state(STATE_CONFIGURATIONS);
+                        }
+                        break;
+                    case STATE_DEPENDENCIES:
+                        if (parent_state() == STATE_ROOT) {
+                            crea->current_dependency = strdup(event.data.scalar.value);
+                            push_state(STATE_DEPENDENCY);
+                        }
+                        break;
+                    case STATE_DEPENDENCY:
+                        if (parent_state() == STATE_DEPENDENCIES) {
+                            add_to_dependencies(
+                                    &crea->dependencies,
+                                    create_dependency(
+                                            crea->current_dependency,
+                                            event.data.scalar.value));
+                        }
+                        FREE_UNLESS_NULL(crea->current_dependency);
+                        crea->current_dependency = NULL;
+                        pop_state();
+                        break;
+                    case STATE_EXTERNAL_DEP:
+                        if (parent_state() == STATE_DEPENDENCY) {
+                            if (!strcmp(event.data.scalar.value, "file")) {
+                                push_state(STATE_FILE);
+                            }
+                        }
+                        break;
+                    case STATE_FILE:
+                        if (parent_state() == STATE_EXTERNAL_DEP) {
+                            add_to_dependencies(
+                                    &crea->dependencies,
+                                    create_external_dependency(
+                                            crea->current_dependency,
+                                            event.data.scalar.value));
+                            FREE_UNLESS_NULL(crea->current_dependency);
+                            crea->current_dependency = NULL;
+                            pop_state();
                         }
                         break;
                     case STATE_NAME:
@@ -273,6 +438,10 @@ parse_file(const char *file)
             yaml_event_delete(&event);
         }
     } while (event.type != YAML_STREAM_END_EVENT);
+    if (last_scalar != NULL) {
+        free(last_scalar);
+        last_scalar = NULL;
+    }
     yaml_event_delete(&event);
 finish:
     yaml_parser_delete(&parser);
@@ -288,6 +457,21 @@ print_sources(source_t *sources)
 
     for (tmp = sources; tmp != NULL; tmp = tmp->next) {
         printf("  %s\n", tmp->path);
+    }
+}
+
+static void
+print_dependencies(dependency_t *deps)
+{
+    dependency_t *tmp = NULL;
+
+    for (tmp = deps; tmp != NULL; tmp = tmp->next) {
+        printf("  %s:\n", tmp->name);
+        if (!tmp->is_external) {
+            printf("    version: %s\n", tmp->version);
+        } else {
+            printf("    file: %s\n", tmp->external.file);
+        }
     }
 }
 
@@ -349,6 +533,8 @@ print_info(crea_t *crea)
     LOGI("Headers:\n");
     print_sources(crea->headers);
     LOGI("Cflags:\n  %s\n", crea->cflags);
+    LOGI("Dependencies:\n");
+    print_dependencies(crea->dependencies);
 }
 
 int
